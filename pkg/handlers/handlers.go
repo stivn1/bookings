8007900d3f53fc73155c950820c3cdb102d487eb
Stiven <majestiic-10@hotmail.com>
2023-02-09 17:03:29 -0500

    Add base handlers and home handler

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,1 +1,29 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/stivn1/bookings/pkg/config"
+	"github.com/stivn1/bookings/pkg/models"
+	"github.com/stivn1/bookings/pkg/render"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home_page.gohtml", &models.TemplatesData{})
+}
